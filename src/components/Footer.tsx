74a405dc71a0b80994c1ea71ca9de06cddd3977a
Alexander <alexander.work.mailbox@gmail.com>
2024-03-01 19:56:27 +0400

    Added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import Logo from "./Logo";
+import Nav from "./Nav";
+import Social from "./Social";
+
 function Footer() {
 	return (
-		<footer>
-			Footer
+		<footer className="py-3 border-t border-solid border-gray-200 bg-[#f2f2f2]">
+			<div className="container">
+				<div className="flex justify-between items-center mb-2">
+					<Logo />
+					<Nav />
+					<Social />
+				</div>
+				<div className="flex justify-center">
+					<p className="text-sm text-gray-400 text-center">
+						© Phraser by alexReactDev
+					</p>
+				</div>
+			</div>
 		</footer>
 	)
 }
---
+++ src/components/Social.tsx
@@ -0,0 +1,28 @@
+function Social() {
+	return (
+		<ul className="flex gap-3">
+			<li className="list-none w-[26px] h-[28px]">
+				<a href="https://www.facebook.com">
+					<img src="/images/icons/facebook.png" alt="facebook" />
+				</a>
+			</li>
+			<li className="list-none w-[26px] h-[28px]">
+				<a href="https://www.instagram.com">
+					<img src="/images/icons/instagram.png" alt="instagram" />
+				</a>
+			</li>
+			<li className="list-none w-[26px] h-[28px]">
+				<a href="https://twitter.com">
+					<img src="/images/icons/twitter.png" alt="twitter (X)" />
+				</a>
+			</li>
+			<li className="list-none w-[26px] h-[28px]">
+				<a href="https://discord.com/">
+					<img src="/images/icons/discord.png" alt="twitter (X)" />
+				</a>
+			</li>
+		</ul>
+	)
+}
+
+export default Social;
