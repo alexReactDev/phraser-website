6f60b2ca880dd3a2ad7b1785cde9aae09b934866
Alexander <alexander.work.mailbox@gmail.com>
2024-03-01 21:36:47 +0400

    Fixed nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,17 +2,17 @@ function Nav() {
 	return (
 		<ul className="grow md:grow-0 justify-evenly flex flex-col items-stretch md:items-center text-gray-700 gap-[10px] md:flex-row md:gap-6">
 			<li className="flex justify-center items-center grow list-none active:bg-gray-100 md:active:bg-inherit">
-				<a href="#home" className="hover:text-[#555] active:text-fontColor-0">
+				<a href="/#home" className="hover:text-[#555] active:text-fontColor-0">
 					Home
 				</a>
 			</li>
 			<li className="flex justify-center items-center grow list-none active:bg-gray-100 md:active:bg-inherit">
-				<a href="#features" className="hover:text-[#555] active:text-fontColor-0">
+				<a href="/#features" className="hover:text-[#555] active:text-fontColor-0">
 					Features
 				</a>
 			</li>
 			<li className="flex justify-center items-center grow list-none active:bg-gray-100 md:active:bg-inherit">
-				<a href="#download" className="hover:text-[#555] active:text-fontColor-0">
+				<a href="/#download" className="hover:text-[#555] active:text-fontColor-0">
 					Download
 				</a>
 			</li>
